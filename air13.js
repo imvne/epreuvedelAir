bc0ba742adb460a8e0d9a4547f83342f03882812
imvne <imanebeaufils19@gmail.com>
2024-06-08 15:05:37 +0200

    metabinks

--- air13.js
+++ air13.js
@@ -1,33 +1,14 @@
 // Meta exercice
 
-// Useful functions
+// Useful functions - colored outputs functions
 
-function slice(arguments, firstIndex, endIndex = arguments.length-1){
-      if (Array.isArray(arguments)){
-            let argumentsSliced = [];
-            for (let i = firstIndex ; i <= endIndex ; i++){
-                  argumentsSliced.push(arguments[i])
-            }
-            return argumentsSliced
-      }
-      else {
-            let argumentsSliced = "";
-            for (let i = firstIndex ; i <= endIndex ; i++){
-                  argumentsSliced += arguments[i]
-            }
-            return argumentsSliced
-      }
+function testAllColors() {
+	for (let i = 0; i < 256; i++) {
+		const colorCode = `\x1b[38;5;${i}m`; // Code d'échappement pour la couleur
+		const colorCodeExample = `'\\x1b[38;5;${i}m'`;
+		console.log(`${colorCode}☗☗☗☗☗☗☗☗☗☗☗☗☗☗\x1b[0m`, colorCodeExample);
+	}
 }
-
-// function testAllColors() {
-// 	for (let i = 0; i < 256; i++) {
-// 	    const colorCode = `\x1b[38;5;${i}m`; // Code d'échappement pour la couleur
-// 	    const colorCodeExample = `'\\x1b[38;5;${i}m'`;
-// 	    console.log(`${colorCode}☗☗☗☗☗☗☗☗☗☗☗☗☗☗\x1b[0m`, colorCodeExample);
-// 	}
-// }
-
-// testAllColors();
 
 function coloredOutput(array){
 	const pastelGreen = `\x1b[38;5;84m`;
@@ -46,6 +27,21 @@ function coloredOutput(array){
 		coloredOutput += "\n"
 	}
 	return coloredOutput;
+}
+
+function coloredOutputTotalSuccesses(string){
+	const pastelGreen = `\x1b[38;5;84m`;
+	const foncePink = `\x1b[38;5;125m`;
+	const endAnsi = '\x1b[0m';
+	
+	let numbers = string.replace(/[^\d]/g, ' ').trim().split(/\s+/);
+	
+	if (numbers[0] < Math.round(numbers[1] / 2)){
+		return `${foncePink}${string}${endAnsi}`
+	}
+	else {
+		return `${pastelGreen}${string}${endAnsi}`
+	}
 }
 
 function multiColorOutput(array){
@@ -79,6 +75,25 @@ function multiColorOutput(array){
 	return coloredOutput;
 }
 
+// Useful functions
+
+function slice(arguments, firstIndex, endIndex = arguments.length-1){
+      if (Array.isArray(arguments)){
+            let argumentsSliced = [];
+            for (let i = firstIndex ; i <= endIndex ; i++){
+                  argumentsSliced.push(arguments[i])
+            }
+            return argumentsSliced
+      }
+      else {
+            let argumentsSliced = "";
+            for (let i = firstIndex ; i <= endIndex ; i++){
+                  argumentsSliced += arguments[i]
+            }
+            return argumentsSliced
+      }
+}
+
 function getFilesList(){ // tableau des fichiers que l'on veut tester
 	const { execSync } = require('child_process')
 	try{
@@ -105,7 +120,7 @@ function executeFileAndGetOutput(fileName) { //output du fichier testé
 		// Exécute le fichier synchroniquement et récupère l'output
 		const output = execSync(`node ${fileName}`, { encoding: 'utf-8' });
 
-		return fileName.includes('air11.js') ? slice(output, 0, output.length-2) : output.trim()
+		return fileName.includes('air11.js') ? slice(output, 0, output.length-2) : output.trim() // exception pour le fichier air11.js qui affiche un pyramide avec des espaces
 	} 
 	
 	catch (error) {
@@ -115,20 +130,17 @@ function executeFileAndGetOutput(fileName) { //output du fichier testé
 }
 
 
-function testsFiles(){
-	const pastelGreen = `\x1b[38;5;84m`;
-	const foncePink = `\x1b[38;5;125m`;
-	
+function testFiles(){
 	let filesToTest = getFilesList()
 	let testedFiles = [];
-	let totalTested = 0;
-	let totalSuccess = 0;
+	let totalTested = 0; // +1 chaque fois qu'un test est fait
+	let totalSuccess = 0; // +1 chaque fois qu'un test est réussi
 	
-	for (let i = 0 ; i < Object.keys(filesInputsOutputs).length ; i++){ // i : 
+	for (let i = 0 ; i < Object.keys(filesInputsOutputs).length ; i++){ // i : fichier JS à tester, récupérés de la fonction getFilesList
 		
-		for (let j = 1 ; j <= Object.keys(filesInputsOutputs[filesToTest[i]]).length; j++){
+		for (let j = 1 ; j <= Object.keys(filesInputsOutputs[filesToTest[i]]).length; j++){ // j : tests de chaque fichier JS, récupérés de l'objet filesInputsOutputs
 			
-			if (executeFileAndGetOutput(`${filesToTest[i]}.js ${filesInputsOutputs[filesToTest[i]]['test' + j].input}`) === filesInputsOutputs[filesToTest[i]]['test' + j].expectedOutput){
+			if (executeFileAndGetOutput(`${filesToTest[i]}.js ${filesInputsOutputs[filesToTest[i]]['test' + j].input}`) === filesInputsOutputs[filesToTest[i]]['test' + j].expectedOutput){ // si le résultat de la fonction qui éxecute et stocke l'output et strictement égal à l'output attendu lui stocké dans l'objet filesInputsOutputs, alors c'est un test réussi
 				testedFiles.push(`${filesToTest[i]} (${j}/${Object.keys(filesInputsOutputs[filesToTest[i]]).length}) : success`)
 				totalSuccess++
 				totalTested++
@@ -151,12 +163,12 @@ let filesInputsOutputs = {
 	air00 : {
 		test1 : { 
 			input : '"Bonjour les gars"',
-			expectedOutput : ["Bonjour", "les", "gars"].join('\n'), 
+			expectedOutput : `Bonjour\nles\ngars`, 
 		},
 		
 		test2 : { 
 			input : '"coucou les meufs"',
-			expectedOutput : ["coucou", "les", "meufs"].join('\n'), 
+			expectedOutput : `coucou\nles\nmeufs`, 
 		},
 		
 		test3 : { 
@@ -169,7 +181,7 @@ let filesInputsOutputs = {
 	air01 : {
 		test1 : { 
 			input : '"Crevette magique dans la mer des étoiles" "la"',
-			expectedOutput : ["Crevette magique dans ", " mer des étoiles"].join('\n'), 
+			expectedOutput : `Crevette magique dans \n mer des étoiles`, 
 		},
 		
 		test2 : { 
@@ -181,15 +193,179 @@ let filesInputsOutputs = {
 	
 	air02 : {
 		test1 : { 
-			input : '"je" "teste" "des" "trucs" " "',
-			expectedOutput : "Je teste des trucs", 
+			input : '"je" "suis" "mon" "âme" " "',
+			expectedOutput : "Je suis mon âme", 
 		},
 		
 		test2 : { 
 			input : 'Je',
 			expectedOutput : "erreur : insérez au moins deux arguments", 
-		}
+		},
 		
+		test3 : { 
+			input : '"êtes" "vous" "votre" "âme" " "',
+			expectedOutput : "Êtes vous votre âme", 
+		},
+	},
+	
+	air03 : {
+		test1 : { 
+			input : 'je suis je',
+			expectedOutput : "suis", 
+		},
+		
+		test2 : { 
+			input : '1 2',
+			expectedOutput : "erreur : insérez au moins trois arguments", 
+		},
+		
+		test3 : { 
+			input : '2 3 4 5 6 5 4 3 2',
+			expectedOutput : "6", 
+		},
+		
+		test4 : { 
+			input : '19 18 19 18 0 -1',
+			expectedOutput : "0 -1", 
+		},
+	},
+	
+	air04 : {
+		test1 : { 
+			input : '"Notre eesprit est une terre fertilee,, sur laquelle tout peut pousser"',
+			expectedOutput : "Notre esprit est une tere fertile, sur laquele tout peut pouser", 
+		},
+		
+		test2 : { 
+			input : '',
+			expectedOutput : "erreur : insérez un argument", 
+		},
+	},
+	
+	air05 : {
+		test1 : { 
+			input : '1 2 3 4 5 "+2"',
+			expectedOutput : "3 4 5 6 7", 
+		},
+		
+		test2 : { 
+			input : '+1',
+			expectedOutput : "erreur : insérez au moins deux arguments", 
+		},
+	},
+	
+	air06 : {
+		test1 : { 
+			input : "'cest' 'génial' 'quand' 'ça' 'marche' 'a'",
+			expectedOutput : "cest", 
+		},
+		
+		test2 : { 
+			input : "'Abdou' 'Ibou' 'Bintou' 'Fatou' 'a'",
+			expectedOutput : "Ibou, Bintou", 
+		},
+		
+		test3 : { 
+			input : 'oui',
+			expectedOutput : "erreur : insérez au moins deux arguments", 
+		},
+	},
+	
+	air07 : {
+		test1 : { 
+			input : "1 5 7 3",
+			expectedOutput : "1 3 5 7", 
+		},
+		
+		test2 : { 
+			input : '1 2',
+			expectedOutput : "erreur : insérez au moins trois arguments", 
+		},
+	},
+	
+	air08 : {
+		test1 : { 
+			input : "10 20 30 fusion 15 25 35",
+			expectedOutput : "10 15 20 25 30 35", 
+		},
+		
+		test2 : { 
+			input : '1 2',
+			expectedOutput : "erreur : insérez au moins trois arguments", 
+		},
+		
+		test3 : { 
+			input : "10 20 30 15 25 35",
+			expectedOutput : "erreur : séparez les arguments par 'fusion'", 
+		},
+		
+		test4 : { 
+			input : "10 20 fusion -45.6",
+			expectedOutput : "erreur : n'insérez que des entiers", 
+		},
+	},
+	
+	air09 : {
+		test1 : { 
+			input : "'Ahmed' 'Karim' 'Thérèse' 'Aïcha'",
+			expectedOutput : "Karim, Thérèse, Aïcha, Ahmed", 
+		},
+		
+		test2 : { 
+			input : '1 5 6 7 8',
+			expectedOutput : "5, 6, 7, 8, 1", 
+		},
+		
+		test3 : { 
+			input : "Imane",
+			expectedOutput : "erreur : insérez au moins deux arguments", 
+		},
+	},
+	
+	air10 : {
+		test1 : { 
+			input : "air10.txt",
+			expectedOutput : `Je pense depuis longtemps déjà que si un jour les méthodes \nde destruction de plus en plus efficaces finissent par \nrayer notre espèce de la planète, ce ne sera pas la \ncruauté qui sera la cause de notre extinction, et \nmoins encore, bien entendu l'indignitation qu' \néveille la cruauté, ni même les représailles \net la vengance qu'elle s'attire... mais la \ndocilité, l'absence, de responsabilité \nde l'homme moderne, son acceptation \nvile et sevile du moindre décret \npublic. Les horreurs auxquelles \nnous allons maintenant assi-\nster ne signalent pas que \nles rebelles, les insu-\nbordonnés, les réfra-\nctaires sont de plus \nen plus nombreux \ndans le monde, \nmais plutôt qu'il y a de plus en plus d'hommes obéissants \n\t\t\t\t\t\tet dociles.\nGoerges Bernanos`, 
+		},
+		
+		test2 : { 
+			input : '',
+			expectedOutput : "erreur : insérez un argument", 
+		},
+	},
+	
+	air11 : {
+		test1 : { 
+			input : "A 5",
+			expectedOutput : "    A    \n   AAA   \n  AAAAA  \n AAAAAAA \nAAAAAAAAA", 
+		},
+		
+		test2 : { 
+			input : 'O',
+			expectedOutput : "erreur : insérez deux arguments", 
+		},
+		
+		test3 : { 
+			input : "P -2",
+			expectedOutput : "erreur : insérez un nombre entier positif en second argument", 
+		},
+	},
+	
+	air12 : {
+		test1 : { 
+			input : "13 19 8 9 7 1",
+			expectedOutput : "1 7 8 9 13 19", 
+		},
+		
+		test2 : { 
+			input : '19',
+			expectedOutput : "erreur : insérez au moins deux arguments", 
+		},
+		
+		test3 : { 
+			input : '1 2 3.5',
+			expectedOutput : "erreur : n'insérez que des nombres entiers", 
+		},
 	},
 	
 }
@@ -198,7 +374,10 @@ let filesInputsOutputs = {
 // Solving
 
 function getResultsTestsFiles(){
-      return coloredOutput(testsFiles()[0]) + '\n' + testsFiles()[1]
+	let filesTestsCompleted = testFiles()
+	
+      return coloredOutput(filesTestsCompleted[0]) + '\n' + coloredOutputTotalSuccesses(filesTestsCompleted[1]) 
+	// 0 contient tout les tests et 1 contient la ligne total des tests réussis
 }
 
 
